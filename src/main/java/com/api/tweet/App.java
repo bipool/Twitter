eada2f1c3d3bf62e325e80c3da16aa8562a46c84
Bipool <bipool@granburyrs.com>
2015-08-09 00:59:54 -0500

    initial commit

---
+++ src/main/java/com/api/tweet/App.java
@@ -0,0 +1,17 @@
+package com.api.tweet;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Twitter twitter = new TwitterFactory().getInstance();
+    	
+    }
+}
